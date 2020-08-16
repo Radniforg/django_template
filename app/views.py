1cc66bcd090e0715a1ba515690cf1f53f49102aa
Ivan Alexandrov <radniforg@yandex.ru>
2020-08-16 20:43:39 +0300

    working version

--- app/views.py
+++ app/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
+import csv
 
 def inflation_view(request):
     template_name = 'inflation.html'
-
+    with open('inflation_russia.csv') as data:
+        reader = csv.DictReader(data)
+        inflation_list = []
+        headers = []
+        for row in reader:
+            for key, value in row.items():
+                if 'Суммарная' not in headers:
+                    headers.append(key)
+                if value == '':
+                    row[key] = '-'
+                else:
+                    if type(value) is str and key != 'Год':
+                        row[key] = float(value)
+            inflation_list.append(row)
     # чтение csv-файла и заполнение контекста
-    context = {}
+    context = {
+        'inflation': inflation_list,
+        'headers': headers
+    }
 
     return render(request, template_name,
                   context)
